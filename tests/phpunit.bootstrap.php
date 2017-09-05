2ed9352fc1c0b2353bb4f14ac029e42ab2d66d0a
Herberto Graca <herberto.graca@gmail.com>
2017-09-05 17:50:41 +0200

    setup containerized unit testing

---
+++ tests/phpunit.bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+(function(): void {
+    require_once __DIR__ . '/../vendor/autoload.php';
+    $secondsToSleep = 3;
+    echo "Waiting ${secondsToSleep}s for all containers to be available...\n";
+    sleep($secondsToSleep);
+})();
